1b9b62b2f9ae2e08b4dfd66125fcafc37bf3cfa6
alvaro <alvaro.perez.trasancos@gmail.com>
2022-07-23 14:46:42 +0200

    Word vectorizer from scracth, git init

---
+++ word_vectorizer/skipgram.py
@@ -0,0 +1,10 @@
+# Simple skipgram
+# Corpus: Legal Dataset -> https://archive.ics.uci.edu/ml/machine-learning-databases/00239/corpus.zip
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('')
+
+
+
